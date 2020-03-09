0f825f3e212552922c27647cf2c27592fc7e6937
shenzb <shenzhibing@shein.com>
2020-03-09 14:51:19 +0800

    - Update declaration of license.

--- src/main/java/io/cruder/excellent/hssf/XlsReader.java
+++ src/main/java/io/cruder/excellent/hssf/XlsReader.java
@@ -1,9 +1,24 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package io.cruder.excellent.hssf;
 
 import io.cruder.excellent.AbstractExcelReader;
-import io.cruder.excellent.util.Reflects;
 import io.cruder.excellent.hssf.eventusermodel.HssfEventFactory;
 import io.cruder.excellent.hssf.eventusermodel.HssfRequest;
+import io.cruder.excellent.util.Reflects;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.hssf.eventusermodel.EventWorkbookBuilder.SheetRecordCollectingListener;
 import org.apache.poi.hssf.eventusermodel.FormatTrackingHSSFListener;
@@ -94,7 +109,7 @@ public class XlsReader<T> extends AbstractExcelReader<T> implements Cloneable {
      *
      * @param filename The file to process
      * @param clazz    parameterized type
-     * @throws IOException
+     * @throws IOException IO exception
      */
     public XlsReader(String filename, Class<T> clazz) throws IOException {
         this(new POIFSFileSystem(new FileInputStream(filename)), clazz);
--- src/main/java/io/cruder/excellent/hssf/eventusermodel/HssfEventFactory.java
+++ src/main/java/io/cruder/excellent/hssf/eventusermodel/HssfEventFactory.java
@@ -1,3 +1,18 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package io.cruder.excellent.hssf.eventusermodel;
 
 
@@ -133,10 +148,9 @@ public class HssfEventFactory {
      *
      * @param req an Instance of HSSFRequest which has your registered listeners
      * @param in  a DocumentInputStream obtained from POIFS's POIFSFileSystem object
-     * @return numeric user-specified result code.
      * @see org.apache.poi.poifs.filesystem.POIFSFileSystem#createDocumentInputStream(String)
      */
-    private short genericProcessEvents(HssfRequest req, InputStream in)
+    private void genericProcessEvents(HssfRequest req, InputStream in)
             throws HSSFUserException {
 
         short userCode = 0;
@@ -166,7 +180,6 @@ public class HssfEventFactory {
         }
 
         // All done, return our last code
-        return userCode;
     }
 
     /**
